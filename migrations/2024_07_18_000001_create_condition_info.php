bd7016a6dde39fa9768af7163aa7c6d0579c2a74
xypp <2952795729@qq.com>
2024-07-24 14:56:26 +0800

    修复错误的列名

--- migrations/2024_07_18_000001_create_condition_info.php
+++ migrations/2024_07_18_000001_create_condition_info.php
@@ -7,7 +7,7 @@ use Flarum\Database\Migration;
 return Migration::createTable(
     'quest_condition_quest_info',
     function (Blueprint $table) {
-        $table->string('quest_condition_name');
+        $table->string('condition_name');
         $table->integer('quest_info_id')->unsigned();
         $table->foreign('quest_info_id')->references('id')->on('quest_info')->onDelete('cascade');
     }
